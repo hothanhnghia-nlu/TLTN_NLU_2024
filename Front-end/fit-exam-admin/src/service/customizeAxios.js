a7c92707f9d5b08a24b75f41062c593c9fabb6ad
hothanhnghia-nlu <nghiant369@gmail.com>
2024-05-12 11:02:25 +0700

    Create upload image and Export file to CSV

--- Front-end/fit-exam-admin/src/service/customizeAxios.js
+++ Front-end/fit-exam-admin/src/service/customizeAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://localhost:44351/api/'
+    baseURL: 'http://192.168.1.25/api/'
 });
 
 // Add a response interceptor
